d8a845fd50b003903c5b672b9213cbd588c9cae0
Cort Fritz <cortfritz@Corts-MacBook-Pro.local>
2022-09-01 14:49:58 -0700

    finished first working draft

---
+++ client/lib/cli.ex
@@ -0,0 +1,83 @@
+defmodule Client.CLI do
+  def main([]) do
+    raise "you must supply an operation and a word e.g. \"$ client insert foo\" where \"client\" is the cli, \"insert\" is the operation and \"foo\" is the word."
+  end
+  def main([_ | [ _ | overflow ]])
+    when overflow != [] do
+    raise "too many arguments: client accepts an operation and a single word."
+  end
+
+  # TODO: protect against words longer than 189,819 characters
+  # TODO: protect against special characters e.g. with...
+      # s
+      # |> String.to_charlist()
+      # |> Enum.filter(&(&1 in 0..127))
+      # |> List.to_string
+
+  def main([operation | args ]) do
+    process(operation, args )
+  end
+
+
+  def process(_, []) do
+    raise "You must supply a word, try \"foo\".  Quotes are optional."
+  end
+
+  def process(operation, args) do
+
+    [ word | _ ] = args
+
+    open_file()
+    |> tx(operation, word)
+    |> conclude
+  end
+
+
+
+  def tx(file, "insert", word) do
+    result = case :dets.insert(file, {word, :valid}) do
+      :ok -> 0
+      _ -> nil
+    end
+    [file, result]
+  end
+
+  def tx(file, "search", word) do
+    result = case :dets.lookup(file, word) do
+      [{_, :valid} | _ ] -> 0
+      _ -> nil
+    end
+
+    [file, result]
+  end
+
+  def tx(file, "remove", word) do
+    result = case :dets.delete(file, word) do
+      :ok -> 0
+      _ -> nil
+    end
+    [file, result]
+  end
+
+  def tx(_, _, _) do
+    raise "operation not supported. please use insert, search, or remove."
+  end
+
+
+  def open_file() do
+    handle = :word_data
+    name = handle
+    :dets.open_file(handle, [{:file, name}])
+    handle
+  end
+
+
+  def conclude([file, result]) do
+    close_file(file)
+    if result, do: IO.puts(to_string(result))
+  end
+
+  def close_file(file) do
+    :dets.close(file)
+  end
+end
--- client/mix.exs
+++ client/mix.exs
@@ -7,6 +7,7 @@ defmodule Client.MixProject do
       version: "0.1.0",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
+      escript: [main_module: Client.CLI],
       deps: deps()
     ]
   end
